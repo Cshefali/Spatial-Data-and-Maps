0fabca9753ad6b1ce3b6b3d3724007f6ced2b77d
Shefali Chandra <shefali.chandra98@gmail.com>
2023-09-19 01:02:47 +0530

    update umemployment-EU script

--- eu-unemployment_rate_map.R
+++ eu-unemployment_rate_map.R
@@ -1,16 +1,17 @@
-#EU map for unemployment rates
-#Last Update- Sept 18, 2023
+#Regional Unemployment Rates across EU member states in 2022
+#Original blog- https://ec.europa.eu/eurostat/statistics-explained/index.php?title=Unemployment_statistics_at_regional_level#Regional_unemployment_rates
+#Last Update- Sept 19, 2023
 #Author- Shefali C.
 
-#code for unemployment rate- 'UNE_RT_M'
+#code for unemployment rate- 'lfst_r_lfu3rt'
 
 library(tidyverse)
 library(giscoR)
 library(eurostat)
 library(sf)
 library(scales)
+library(lubridate)
 
-lfst_r_lfu3rt
 
 working_dir <- getwd()
 
@@ -18,15 +19,20 @@ working_dir <- getwd()
 set_eurostat_cache_dir(cache_dir = file.path(working_dir, "data/eu-unemployment"),
                        install = T)
 
-unemployment_data <- eurostat::get_eurostat(id = "lfst_r_lfu3rt",
-                                            type = "label")
+#in this dataframe, NUTS-2 regions are represented by 'geo'
+data_with_code <- eurostat::get_eurostat(id = "lfst_r_lfu3rt")
 
-#proper NUTS-2 codes
-unemployment_data2 <- eurostat::get_eurostat(id = "lfst_r_lfu3rt")
+#let's see the time range of this data
+summary(data_with_code$time)
 
-#filter data for years 2022 and 2023
-data_2022_23 <- unemployment_data[grep(pattern = "2022|2023", unemployment_data$time),]
+unique(data_with_code$geo)
 
-#EU Area
-eu_area <- unemployment_data[grep(pattern = "Euro area", unemployment_data$geo),]
-eu_area_total <- eu_area[eu_area$age == "Total",]
+#convert time column to date format
+#data_with_code$time <- as_date(data_with_code$time)
+
+
+#filter data for the year 2022
+data_2022 <- data_with_code %>% filter(year(time) == 2022)
+
+#filter data with EU27_2020
+eu_2020 <- data_with_code[data_with_code$geo == "EU27_2020",]
